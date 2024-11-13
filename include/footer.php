cbbab7468bb124651f9106c718c95eb0708ee53c
Nazia Nuzhat <37345962+Nazia45@users.noreply.github.com>
2024-11-13 21:29:45 +0600

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,17 @@
+<style>
+	.copyrightText{
+  width: 100%;
+  background: #181818;
+  padding: 8px 100px;
+  text-align: center;
+  color: #999;
+  font-size: 15.5px;
+}
+</style>
+			<footer>
+				<div class="copyrightText">
+					<p>
+						Copyright © 2022 By MediAid. All Rights Reserved.
+					</p>
+				</div>
+			</footer>
